a979c3e29c6956631742c6d34bf2f6ef2d59c7e5
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-13 04:08:54 -0400

    create my own workspace

---
+++ myProject/ch1_simpleSetup/client/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+    return (
+        <div>
+          <h1>Hello World!</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates corporis reiciendis magni ex quibusdam minus numquam nostrum dolor quod animi praesentium illum, hic itaque, delectus nobis perferendis? Quo, dignissimos consequuntur?</p>
+        </div>
+      )
+}
+
+export default hot(module)(HelloWorld)
